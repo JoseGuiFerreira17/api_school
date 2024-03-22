16d45f6710910497ab590217e322576b1c8e0038
guilherme <joseguiferreira17@gmail.com>
2024-03-22 18:40:42 -0300

    criação de funcionalidade para gerar matricula

---
+++ apps/student/api/serializers/__init__.py
@@ -0,0 +1,4 @@
+from apps.student.api.serializers.student import StudentCreateSerializer, StudentDetailSerializer, StudentListSerializer
+
+
+__all__ = ["StudentCreateSerializer", "StudentDetailSerializer", "StudentListSerializer"]
--- apps/student/models/student.py
+++ apps/student/models/student.py
@@ -8,6 +8,8 @@ from django.db.models import (
     FileField,
     CASCADE,
 )
+from datetime import datetime
+from hashlib import sha256
 from apps.core.utils import GENDER_CHOICE
 
 
@@ -21,12 +23,12 @@ def upload_to_cpf(instance, filename):
 
 class Student(Model):
     id = UUIDField("ID", primary_key=True, default=uuid.uuid4, editable=False)
-    registration = CharField("Matrícula", max_length=10, null=True, blank=True)
+    registration = CharField("Matrícula", max_length=30, unique=True, null=True, blank=True)
     name = CharField("Nome", max_length=100)
     gender = CharField("Sexo", max_length=9, choices=GENDER_CHOICE)
     birth_date = DateField("Data de Nascimento")
     rg = CharField("RG", max_length=15, null=True, blank=True)
-    cpf = CharField("CPF", max_length=14)
+    cpf = CharField("CPF", unique=True, max_length=14)
     parent = ForeignKey(
         "student.Parent", verbose_name="Informações dos responsáveis", on_delete=CASCADE
     )
@@ -41,6 +43,12 @@ class Student(Model):
         return (
             self.name if not self.registration else f"{self.registration}-{self.name}"
         )
+    
+    def save(self, *args, **kwargs):
+        cpf = sha256(self.cpf.replace(".", "").replace("-", "").encode()).hexdigest()
+        registrantion = f"{datetime.now().year}{cpf[:8].upper()}"
+        self.registration = registrantion
+        super().save()
 
     @property
     def age(self):
